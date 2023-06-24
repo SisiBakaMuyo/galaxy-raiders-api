433ece9c97f0b95862cf7320dcef8691235eff4c
Patty <107845984+patriciarodrigues151@users.noreply.github.com>
2023-06-24 11:44:47 -0300

    Add files via upload
    adicionei o Explosion.kt

---
+++ app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
@@ -0,0 +1,23 @@
+
+package galaxyraiders.core.game
+
+import galaxyraiders.core.physics.Point2D
+import galaxyraiders.core.physics.Vector2D
+
+class Explosion(initialPosition: Point2D, radius: Double, timeToLive: Double) :
+  SpaceObject("Explosion", '*', initialPosition, Vector2D(0.0, 0.0), radius, mass = 0.0) {
+
+  var isTriggered = true
+  var timeRemaining = timeToLive
+
+  fun update() {
+    if (this.timeRemaining <= 0.0) {
+      this.isTriggered = false
+    }
+    this.timeRemaining -= 1.0
+  }
+
+  fun isTriggeredExplosion(): Boolean {
+    return this.isTriggered
+  }
+}
